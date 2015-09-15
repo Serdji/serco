957792d6ea8df09ca85163471d7d8c55e78bfd64
serdji <c.c@bk.ru>
2015-09-15 08:14:10 +0300

    Коммит для теста отката

--- js/main.js
+++ js/main.js
@@ -210,8 +210,11 @@ $( 'a[href="/"]' ).on('click', function() {
 
 	// document.ondragstart = noselect;     // запрет на перетаскивание     
 	document.onselectstart = noselect;     // запрет на выделение элементов страницы     
-	document.oncontextmenu = noselect;     // запрет на выведение контекстного меню     
-	function noselect() {return false;}  
+	document.oncontextmenu = noselect;     // запрет на выведение контекстного меню 
+	    
+	function noselect() {
+		return false;
+	};  
 
 
 /*-------------------------------------------------------------------*/
